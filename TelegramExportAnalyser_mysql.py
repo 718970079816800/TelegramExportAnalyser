697b3845ebe933466c57034f18cd0b1b2379046d
Adam <12171817+718970079816800@users.noreply.github.com>
2020-02-16 19:45:46 +0800

    Update TelegramExportAnalyser_mysql.py

--- TelegramExportAnalyser_mysql.py
+++ TelegramExportAnalyser_mysql.py
@@ -9,12 +9,12 @@
 #Table column:'message':varchar(9999)
 #Table column:'hash':varchar(32)
 
-import codecs, re, os, hashlib
+import codecs, re, os, hashlib, time
 import pymysql
 from bs4 import BeautifulSoup
 from datetime import datetime as dt
 
-TEF = r'D:\TG_Export'
+TEF = r'E:\TG_Export_201906-202002'
 
 def list_html_files(rootdir):
 #Return all html files in a specified dir including sub dir.
@@ -61,68 +61,73 @@ def extract_diaglog(rootdir):
     dup = 0
     err = 0
     for htmlpage in list_html_files(rootdir):
-        soup = BeautifulSoup(open(htmlpage,encoding='utf8'),'lxml')
-        head = soup.find('div', class_="page_header")
         try:
-            grname = head.find('div',class_='text bold').text.strip()
-        except AttributeError:
-            grname = 'None'
-        msgs = soup.find_all(class_=lambda x: x and x.startswith('message default'))
-        for msg in msgs:
-            time = msg.find('div', class_="pull_right date details").get('title')
-            date = dt.strptime(time.split()[0], '%d.%m.%Y').strftime('%Y-%m-%d')
-            hr = dt.strptime(time.split()[1], '%H:%M:%S').strftime('%H:%M:%S')
-            _time = date + ' ' + hr
+            soup = BeautifulSoup(open(htmlpage,encoding='utf8'),'lxml')
+            head = soup.find('div', class_="page_header")
             try:
-                name = msg.find('div', class_="from_name").text.strip()
+                grname = head.find('div',class_='text bold').text.strip()
             except AttributeError:
-                name = 'None'
-            try:
-                text = msg.find('div', class_="text").text.strip()
-            except AttributeError:
-                text = 'None'
+                grname = 'None'
+            msgs = soup.find_all(class_=lambda x: x and x.startswith('message default'))
+            for msg in msgs:
+                _time = msg.find('div', class_="pull_right date details").get('title')
+                date = dt.strptime(_time.split()[0], '%d.%m.%Y').strftime('%Y-%m-%d')
+                hr = dt.strptime(_time.split()[1], '%H:%M:%S').strftime('%H:%M:%S')
+                _time = date + ' ' + hr
+                try:
+                    name = msg.find('div', class_="from_name").text.strip()
+                except AttributeError:
+                    name = 'None'
+                try:
+                    text = msg.find('div', class_="text").text.strip()
+                except AttributeError:
+                    text = 'None'
 
-            _str = ('Time:'+date+' '+hr+'\n'+
-                    'GroupName:'+grname+'\n'+
-                    'Name:'+name+'\n'+
-                    'Message:'+text+'\n')   
-            _hash = hashlib.md5(_str.encode('utf8')).hexdigest()
+                _str = ('Time:'+date+' '+hr+'\n'+
+                        'GroupName:'+grname+'\n'+
+                        'Name:'+name+'\n'+
+                        'Message:'+text+'\n')   
+                _hash = hashlib.md5(_str.encode('utf8')).hexdigest()
 
-            sql = ("INSERT \
-                    INTO \
-                    dialogues(time, \
-                              group_name, \
-                              name, \
-                              message, \
-                              hash) \
-                              VALUES ('%s', '%s',  '%s',  '%s',  '%s')" 
-                              % (_time, \
-                              pymysql.escape_string(grname), \
-                              pymysql.escape_string(name), \
-                              pymysql.escape_string(text), \
-                              _hash))
-            try:
-                db = pymysql.connect(
-                    host="127.0.0.1",
-                    port=3306,
-                    user="root",
-                    passwd="",
-                    db="tg_dialogues",
-                    charset='utf8mb4'
-                    )
-                cursor = db.cursor()
-                cursor.execute(sql)
-                db.commit()
-                db.close()
-                imp += 1
-            except pymysql.err.IntegrityError as e:
-                dup += 1
-                print('Duplicate:', e)
-                db.close()
-            except pymysql.err.OperationalError as e:
-                err += 1
-                print('Error:', e)
-                pass
+                sql = ("INSERT \
+                        INTO \
+                        dialogues(time, \
+                                group_name, \
+                                name, \
+                                message, \
+                                hash) \
+                                VALUES ('%s', '%s',  '%s',  '%s',  '%s')" 
+                                % (_time, \
+                                pymysql.escape_string(grname), \
+                                pymysql.escape_string(name), \
+                                pymysql.escape_string(text), \
+                                _hash))
+                try:
+                    db = pymysql.connect(
+                        host="127.0.0.1",
+                        port=3306,
+                        user="root",
+                        passwd="",
+                        db="tg_dialogues",
+                        charset='utf8mb4'
+                        )
+                    cursor = db.cursor()
+                    cursor.execute(sql)
+                    db.commit()
+                    db.close()
+                    imp += 1
+                except pymysql.err.IntegrityError as e:
+                    dup += 1
+                    print('Duplicate:', e)
+                    db.close()
+                except pymysql.err.OperationalError as e:
+                    err += 1
+                    print('Error:', e)
+                    pass
+        except FileNotFoundError as e:
+            print(e)
+            time.sleep(10)
+            pass
     print('Imported count: ' + str(imp))
     print('Duplicated count: ' + str(dup))
     print('Warning count: ' + str(err))
